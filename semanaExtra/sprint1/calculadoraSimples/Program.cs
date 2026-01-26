bde3a203e1267b84b7427c6a63e38eec3f6f8210
Luís <luisGNS8022@gmail.com>
2026-01-25 23:32:34 -0300

    Exercícios extras

---
+++ semanaExtra/sprint1/calculadoraSimples/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Qual conta você deseja fazer (+, -, *, /)? ");
+string escolha = Console.ReadLine();
+
+string[] operacoes = { "+", "-", "*", "/" };
+
+if (!operacoes.Contains(escolha))
+{
+    Console.WriteLine("Operador inválido!");
+}
+else
+{
+    Console.Write("Escolha seu primeiro número: ");
+    int.TryParse(Console.ReadLine(), out int numero1);
+
+    Console.Write("Escolha seu segundo número: ");
+    int.TryParse(Console.ReadLine(), out int numero2);
+
+    if (numero2 == 0)
+    {
+        Console.WriteLine("Divisão por zero não é permitida.");
+    }
+    else
+    {
+        switch (escolha)
+        {
+            case "+":
+                Console.WriteLine($"Resultado: {numero1 + numero2}");
+                break;
+            case "-":
+                Console.WriteLine($"Resultado: {numero1 - numero2}");
+                break;
+            case "*":
+                Console.WriteLine($"Resultado: {numero1 * numero2}");
+                break;
+            case "/":
+                Console.WriteLine($"Resultado: {numero1 / numero2}");
+                break;
+        }
+    }
+}
+
---
+++ semanaExtra/sprint1/contagemVogais/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] vogais = {"a", "e", "i", "o", "u"};
+
+Console.Write("Digite sua palavra: ");
+string palavra = Console.ReadLine().ToLower();
+
+int encontrada = 0;
+
+foreach (char letra in palavra)
+{
+    if (vogais.Contains(letra.ToString())) // Tratamento de erro ToString
+    {
+        Console.Write($"{letra}, ");
+        encontrada += 1;
+    }
+}
+
+Console.WriteLine($"\nA palavra possui {encontrada} vogais.");
